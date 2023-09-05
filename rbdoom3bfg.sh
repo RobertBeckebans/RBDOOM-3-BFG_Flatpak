09b1245f5fba75a25c67399569d136c5925fe032
mbugni <massi.ergosum@gmail.com>
2023-09-05 20:11:25 +0200

    Fix permissions and setup script

--- rbdoom3bfg.sh
+++ rbdoom3bfg.sh
@@ -5,15 +5,21 @@ rbdoom3bfg_data_dir=${XDG_DATA_HOME}/rbdoom3bfg
 rbdoom3bfg_base_dir=${rbdoom3bfg_data_dir}/base
 
 # Check data folder
+if [ ! -d ${rbdoom3bfg_data_dir} ]
+then
+    mkdir -p ${rbdoom3bfg_data_dir}
+fi
+
+# Check base folder
 if [ ! -d ${rbdoom3bfg_base_dir} ]
 then
-    zenity --width=400 --title="Game data files not found" \
-    --error --no-wrap --text="The /base game folder must be copied to:\n${rbdoom3bfg_base_dir}" \
+    zenity --width=400 --error --title="Game data files not found" \
+    --no-wrap --text="The /base game folder must be copied to:\n${rbdoom3bfg_base_dir}" \
     --ok-label "Close"
     exit 1
 elif [ ! -f ${rbdoom3bfg_data_dir}/.setup ]
 then
-    # Data folder exists, but it is not initialized
+    # Base folder exists, but it is not initialized
     echo "Copying data patches..."
     cp --recursive /app/share/rbdoom3bfg/base/* --target-directory ${rbdoom3bfg_base_dir}
     touch ${rbdoom3bfg_data_dir}/.setup
